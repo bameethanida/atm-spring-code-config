4a253c3be74b33161b5edc8134a0ce38ccff1876
bameethanida <bameejrn@outlook.com>
2020-09-24 18:24:02 +0700

    Change data sources file

--- src/main/java/atm/config/ATMConfig.java
+++ src/main/java/atm/config/ATMConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class ATMConfig {
     @Bean
     public DataSource dataSource() {
-        return new DataSourceDB();
+        return new DataSourceFile("customers.txt");
     }
 
     @Bean
